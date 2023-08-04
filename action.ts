c6a95d79da670de360da3e8f57d7ba5c63ad3c17
Jonathan Delgado <hi@jon.soy>
2023-08-04 01:24:46 -0400

    build: configure blocked step

--- action.ts
+++ action.ts
@@ -1,12 +1,17 @@
 import * as code from "@actions/core";
 import * as github from "@actions/github";
 import { inspect } from "node:util";
+import * as YAML from "yaml";
 import { evaluate } from ".";
 
 const main = async () => {
+  const blockStep: boolean = YAML.parse(code.getInput("blockStep"));
   const option = code.getInput("option");
   const available = evaluate(option);
   code.setOutput("available", available);
+  if (blockStep) {
+    code.setFailed("Is blocked");
+  }
 };
 
 const captureMessage = (ex: unknown): string => {
