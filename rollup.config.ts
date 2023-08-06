66ee2fdf95516b7ac53c65abde4eb1fa5d198a9c
Jonathan Delgado <hi@jon.soy>
2023-08-05 22:36:23 -0400

    feat: configure step update readme

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,11 +2,13 @@ import type { RollupOptions } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 
 const config: RollupOptions = {
-  plugins: [typescript({
-    target: "es2015",
-  })],
+  plugins: [typescript()],
   external: [],
-  input: ["action.ts", "index.ts", "append-supported-tags.ts", "fill-document.ts"],
+  input: [
+    "action.ts",
+    "index.ts",
+    "fill-document.ts",
+  ],
   output: {
     format: "cjs",
     sourcemap: true,
